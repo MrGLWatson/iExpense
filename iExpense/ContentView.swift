8198d1e77c624e0b38073f7595e668dec03aacef
Gary Watson <Gary_Watson@Blueyonder.co.uk>
2020-06-13 11:15:49 +0100

    Initial Commit

---
+++ iExpense/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iExpense
+//
+//  Created by Gary Watson on 13/06/2020.
+//  Copyright © 2020 Gary Watson. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
